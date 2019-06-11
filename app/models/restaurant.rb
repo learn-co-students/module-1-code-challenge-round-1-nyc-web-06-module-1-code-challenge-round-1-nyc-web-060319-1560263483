ae2922e6ca11d867b50e5cf8f29d9fba8d2a4cf1
JakubowskiA <arieljakubowski@gmail.com>
2019-06-11 11:40:20 -0400

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,52 @@
 class Restaurant
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews #all reviews written about this restaurant
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def customers #customers writing all reviews, by review
+    reviews.map do |review|
+      review.customer
+    end.uniq #makes it unique
+  end
+
+  def all_ratings #all reviews for restaurant
+    reviews.map do |review|
+      review.rating
+    end
+  end
+  
+  def average_star_rating #sum and number of ratings, averaged
+    (all_ratings.sum) / (all_ratings.length)
+  end
+
+  def review_content #reviews by content
+    reviews.map do |review|
+      review.content
+    end
+  end
+  
+  def longest_review
+    review_content.sort_by{|content| content.length}
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |restaurant|
+      restaurant.name == name
+    end
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,26 @@
 class Review
-  
+    attr_accessor :content, :rating
+    attr_reader :customer, :restaurant
+    @@all = []
+
+    def initialize(restaurant, content, rating, customer)
+        @restaurant = restaurant
+        @content = content
+
+        if rating.to_f < 1 #setting limits 1-5 for rating
+            @rating = 1
+        elsif rating.to_f > 5
+            @rating = 5
+        else
+            @rating = rating.to_f
+        end
+        @customer = customer
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
 
