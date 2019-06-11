0353be0ca4c616e6da8959ac4bdb09bf27202efa
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-06-11 11:40:05 -0400

    I did Itgit status

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,18 @@
 class Review
-  
+  @@all = []
+  attr_reader :restaurant, :customer
+  attr_accessor :content, :rating
+  def initialize(restaurant, content, rating, customer)
+    @restaurant = restaurant
+    @content = content
+    @rating = rating
+    @customer = customer
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
 
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,21 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+
+c1 = Customer.new("Harpreet", "Ghotra")
+c2 = Customer.new("Chett", "has_last_name")
+c3 = Customer.new("Harpreet", "Singh")
+
+r1 = Restaurant.new("Plaza Deli")
+r2 = Restaurant.new("Burger King")
+
+c1.add_review(r1, "Great Place to eat", 5)
+c1.add_review(r1, "Great Place to eat and making it longer", 4)
+
+c2.add_review(r2, "Good Place to eat", 3)
+
+found_existing_review = Review.all.find{|review| review.restaurant == r1}
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
