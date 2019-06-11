2c65e54894209d10c286fbea7c0ba5e34fffae39
Ingrid04 <ingridfmelgar@gmail.com>
2019-06-11 11:41:04 -0400

    code challenge finished

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,47 @@
+require_relative 'restaurant.rb'
+require_relative 'review.rb'
+
+
 class Customer
   attr_reader :first_name, :last_name
+
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.all.customer.map {|restaurant| restaurant.review == self }
+    review << restaurant.customer
+  end
+
+  #review defined in the other class
+  def num_reviews
+    Review.all.customer.sum
+  end
+
+  def restaurants
+    self.num_reviews.restaurant.each do |restaurant|
+      customer.restaurant.uniq
+  end
+
+  #classmethod
+  def self.find_by_name(name)
+    self.name.first
+
+  end
+
+
 end
+
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,42 @@
+
 class Review
+   attr_reader :customer, :restaurant
+   attr_accessor :content, :rating
+   
+    @@all = []
+
+    def initialize (customer, restaurant, content, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @content = content
+        @rating = rating
+
+        @@all << self
+
+    end
+
+    def self.all
+        @@all 
+    end
+
+    def customer
+        self.customer.restaurant
+
+    end
+
+    def restaurant
+        self.restaurant.customer 
+    end
+
+    def rating
+
+        
+    end
+
+    def content
+        self.content.restaurant
+
+    end
   
 end
 
