151b0361617af30dd204962ea5bc89a9ba5ac82e
alexvirga <github email address>
2019-06-11 11:40:38 -0400

    DId it

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,42 @@
 class Restaurant
-  attr_reader :name
+  attr_reader :name, :content, :rating
 
-  def initialize(name)
+@@all = []
+
+  def initialize(name, content, rating)
     @name = name
+    @content = content
+    @rating = rating
+    @@all << self
   end
+
+
+def customer_sort
+  Review.all.select do |reviews|
+    reviews.restaurant == self
+  end
+end
+
+def customer
+  customer_sort.map do |y|
+    y.customers
+  end
+end
+
+def reviews
+  customer_sort.map do |x|
+    x.review
+  end
+end
+
+
+# def reviews
+
+
+
+def self.all
+  @@all
+end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+
+customer1 = Customer.new("Miss","Waffleson" )
+customer2 = Customer.new("Sharon", "Syrupo")
+customer3 = Customer.new("Dolores", "Jammi")
+
+restaurant1 = Restaurant.new("Outfront", "Austrian", 4)
+restaurant2 = Restaurant.new("Waffle garden", "Breakfast", 5)
+restaurant3 = Restaurant.new("Burger queen", "Nuggets", 3)
+
+review1 = Review.new(restaurant1, customer1, "Ehhhh")
+review2 = Review.new(restaurant2, customer2, "Terrible")
+review3 = Review.new(restaurant3, customer3, "Good")
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
