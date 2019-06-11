563359136d6206af7bf1b2316f73e2de046575de
te68 <te68@cornell.edu>
2019-06-11 11:39:43 -0400

    I DID IT

---
+++ review.rb
@@ -0,0 +1,33 @@
+class Review
+
+attr_accessor :content, :rating
+attr_reader :customer, :restaurant
+
+@@all = []
+
+def initialize(restaurant, content, rating, customer)
+  @restaurant = restaurant
+  @content = content
+  @rating = rating
+  @customer = customer
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
