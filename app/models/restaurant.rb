547e7b48b2627d141b09ef858977efaa1d703aa9
wkpescherine <wkpescherine@gmail.com>
2019-06-11 11:41:37 -0400

    William Pescherine code

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,24 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+  	@@all
+  end	
+
+  def find_by_name(name)
+  	Restaurant.all.find do |restaurant|
+  		restaurant == name
+  end	
+
+  def customers
+  	customers.restaurant.select do |restaurant|
+  		restaurant == self
+  end	
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,36 @@
 class Review
+
+	attr_accessor :rating
+	attr_reader :customer, :restaurant
+
+	@@all = []
+
+	def Initialize(customer, restaurant, rating)
+		@customer = customer
+		@restaurant = restaurant
+
+		if ratings > 5
+			@ratings = 5
+		elsif ratings <1
+			@ratings = 1
+		else
+			@ratings = rating
+
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end	
+
+	def customer(customer)
+		self.customer
+	end
+
+	def content(customer)
+		puts self.content
+	end
   
+
 end
 
